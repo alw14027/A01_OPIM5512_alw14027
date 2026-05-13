78fdd4cf84a19fe07874d2999bcd2637ee7d4d9d
Alaina Washington <alaina.washington@uconn.edu>
2026-05-12 23:49:46 -0400

    Update boxplot.py

--- src/boxplot.py
+++ src/boxplot.py
@@ -18,7 +18,7 @@ plt.figure(figsize=(10, 6))
 df.boxplot(column=['MedInc'])
 plt.title('Distribution of Median Income in California')
 
-# This is the line that handles the magic
+# Save figure in "figs" folder
 plt.savefig('../figs/california_boxplot.png')
 
 plt.show()
